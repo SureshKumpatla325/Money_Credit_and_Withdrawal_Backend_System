49a189e685bd2e1c8095d10d0317f3f83b602725
Suresh Kumpatla <sureshkumpatlar325@gmail.com>
2024-07-25 22:27:21 +0530

    project

---
+++ Money_Credit_and_Withdrawal_Backend_System.py
@@ -0,0 +1,11 @@
+class Money_Credit_and_Withdrawal_System:
+    #pass
+    if __name__ == '__main__':
+        print("Hi welcome to the money Credit and Withdrawal Backend System")
+        print('I am assesting for credit and withdraw money from your bank account')
+        print('''please select the options from below
+               1. Enter 1 for registration 
+               2. Enter 2 for login
+               ''')
+        registration_ligin_cmd = int(input())
+        print(f"{registration_ligin_cmd}")
